fc25c157a5b36117efba973f4fb97f370790aa50
Jogaraodandu <96335777+Jogaraodandu@users.noreply.github.com>
2022-03-03 13:19:56 +0530

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld{
+ public static void main(String args[]){
+System.out.println("Hello Jenkins have a good day  ");
+System.out.println("Hi, My name is Joga  ");
+System.out.println("I am from visakhapatnam ");
+}
+}
